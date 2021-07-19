90ff3dc684d6bed35e9013ff952b57baef75d353
kokili <kokili.developer@outlook.com>
2021-07-19 10:36:47 +0800

    add another solution with callback function

--- Solution_with_Callback/js/callbacks.js
+++ Solution_with_Callback/js/callbacks.js
@@ -1,20 +1,26 @@
+// jshint esversion:6
 const astrosUrl = 'http://api.open-notify.org/astros.json';
 const wikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/';
 const peopleList = document.getElementById('people');
 const btn = document.querySelector('button');
 
+
+
 // Make an AJAX request
-function getJSON(url) {
+function getJSON(url,callback) {
   const xhr = new XMLHttpRequest();
   xhr.open('GET', url);
+  xhr.send();
+
   xhr.onload = () => {
     if(xhr.status === 200) {
       let data = JSON.parse(xhr.responseText);
-      console.log(data);
+      return callback(data);
     }
   };
-  xhr.send();
 }
+
+
 
 // Generate the markup for each profile
 function generateHTML(data) {
@@ -37,3 +43,14 @@ function generateHTML(data) {
     `;
   }
 }
+
+
+
+btn.addEventListener('click', () => {
+  document.querySelector('button').remove();
+  getJSON(astrosUrl,
+    data => {data.people.map(
+    x => {getJSON(wikiUrl + x.name, generateHTML);
+    });
+});
+});
